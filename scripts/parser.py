e0e371388ce0ae4902aba6f49c26a4ab8acaeb9e
padap <padamenko@gmail.com>
2019-04-17 03:08:08 +0300

    1-st commit

---
+++ scripts/parser.py
@@ -0,0 +1,45 @@
+import re
+import requests
+import os, sys
+from tqdm import tqdm, tqdm_notebook
+
+import time
+import youtube_dl
+import pickle
+import shutil
+
+
+RESTTIME = 5
+DATAFOLDER = '../data/'
+if not os.path.exists(DATAFOLDER):
+    os.mkdir(DATAFOLDER)
+    
+def get_urls(fn = 'urls.txt'):
+    lst = open(fn, 'r', encoding="utf-8").read().split('\n')
+    res = {i.split('	')[0]:float(i.split('	')[1]) for i in lst}
+    return res
+
+def load_single_video(url):
+    result = False
+    try:
+        ydl_opts = {}
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                    ydl.download([url])
+        result = True
+    except:
+        sys.stderr.write("Can't load video %s"%url)
+        time.sleep(RESTTIME*30)
+        
+url_list = [i[0] for i in sorted(get_urls().items(), key = lambda x:-x[1])]
+
+for url in tqdm(url_list):
+    files_old = os.listdir('.')
+    load_single_video(url)
+    files_new = os.listdir('.')
+    files = list(set(files_new)-set(files_old))
+    if len(files)!=1:
+        sys.stderr.write('Something wrong, there are several files :\n'+str(files))
+    else:
+        fn = files[0]
+        shutil.move(fn, DATAFOLDER+fn)
+    time.sleep(RESTTIME)
